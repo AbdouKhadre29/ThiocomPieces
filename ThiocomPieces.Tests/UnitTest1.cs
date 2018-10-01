4b0ea5655decf3b5ef83e06f723b49af183812cf
PC <PC@DESKTOP-T130P6I>
2018-10-01 12:58:41 +0200

    Ajoutez des fichiers projet.

---
+++ ThiocomPieces.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThiocomPieces.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
